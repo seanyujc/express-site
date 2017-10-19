30c6fa6f420cab1c4dd8b33a519b360937016c44
yujc <jch_yu@hotmail.com>
2017-10-20 07:07:13 +0800

    返回json

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  const data = {
+    code: "000000",
+    msg: "success",
+    data: ""
+  }
+  res.header("Content-Type", "application/json;charset=UTF-8")
+  res.send(JSON.stringify(data));
 });
 
 module.exports = router;
